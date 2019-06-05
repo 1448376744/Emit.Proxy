53972870b97e0a43ce8c2c86224aac3cf5036907
1448376744 <41883220+1448376744@users.noreply.github.com>
2019-06-05 18:19:27 +0800

    Initial commit

---
+++ IInterceptor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emit.Proxy
+{
+    public interface IInterceptor
+    {
+        void Intercept(IInvocation invocation);
+    }
+}
---
+++ IInvocation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Emit.Proxy
+{
+    public interface IInvocation
+    {
+        object[] Arguments { get; set; }
+        object ReturnValue { get; set; }
+        object Target { get; }
+        MethodInfo TargetMethod { get; }
+        Type TargetType { get; }
+        void Proceed();
+    }
+    public class DefaultInvocation : IInvocation
+    {
+        public object[] Arguments { get; set; }
+        public object Target { get; set; }
+        public MethodInfo TargetMethod { get; set; }
+        public object ReturnValue { get; set; }
+        public Type TargetType { get; set; }
+        public void Proceed()
+        {
+            ReturnValue = TargetMethod.Invoke(Target, Arguments);
+        }
+    }
+
+}
